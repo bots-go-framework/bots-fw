dc61b46f02b7b89f0bbf12eeb71fa8938b66c7aa
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2016-08-18 23:40:05 +0100

    Refactoring

--- core/const.go
+++ core/const.go
@@ -4,15 +4,6 @@ import "errors"
 
 var NotImplementedError = errors.New("Not implemented")
 
-type InviteBy string
-
-const (
-	InviteByTelegram = InviteBy("telegram")
-	InviteByFbm      = InviteBy("fbm")
-	InviteByEmail    = InviteBy("email")
-	InviteBySms      = InviteBy("sms")
-)
-
 const (
 	MESSAGE_TEXT_I_DID_NOT_UNDERSTAND_THE_COMMAND = "MESSAGE_TEXT_I_DID_NOT_UNDERSTAND_THE_COMMAND"
 	MESSAGE_TEXT_OOPS_SOMETHING_WENT_WRONG        = "MESSAGE_TEXT_OOPS_SOMETHING_WENT_WRONG"
