d027499b82a5e3f3020eb51aaeac791b1474d50f
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2017-04-09 22:52:16 +0100

    Added check and panic if context is nil

--- hosts/appengine/logger.go
+++ hosts/appengine/logger.go
@@ -12,22 +12,37 @@ func (l logger) Name() string {
 	return ""
 }
 func (l logger) Debugf(c context.Context, format string, args ...interface{}) {
+	if c == nil {
+		panic("Required parameter 'c context.Context' is nill")
+	}
 	logGae.Debugf(c, format, args...)
 }
 
 func (l logger) Infof(c context.Context, format string, args ...interface{}) {
+	if c == nil {
+		panic("Required parameter 'c context.Context' is nill")
+	}
 	logGae.Infof(c, format, args...)
 }
 
 func (l logger) Warningf(c context.Context, format string, args ...interface{}) {
+	if c == nil {
+		panic("Required parameter 'c context.Context' is nill")
+	}
 	logGae.Warningf(c, format, args...)
 }
 
 func (l logger) Errorf(c context.Context, format string, args ...interface{}) {
+	if c == nil {
+		panic("Required parameter 'c context.Context' is nill")
+	}
 	logGae.Errorf(c, format, args...)
 }
 
 func (l logger) Criticalf(c context.Context, format string, args ...interface{}) {
+	if c == nil {
+		panic("Required parameter 'c context.Context' is nill")
+	}
 	logGae.Criticalf(c, format, args...)
 }
 
