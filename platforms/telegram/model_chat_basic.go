f99d20094742ddc0a1c98288533802522f203c9c
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2018-05-30 22:09:51 +0100

    Basic Telegram chat entity class for reuse by apps without changing

---
+++ platforms/telegram/model_chat_basic.go
@@ -0,0 +1,59 @@
+package telegram
+
+import (
+	"github.com/strongo/db"
+	"github.com/strongo/db/gaedb"
+	"google.golang.org/appengine/datastore"
+)
+
+type Chat struct {
+	TgChatBase
+	*ChatEntity
+}
+
+var _ db.EntityHolder = (*Chat)(nil)
+
+func (Chat) Kind() string {
+	return ChatKind
+}
+
+func (tgChat Chat) Entity() interface{} {
+	return tgChat.ChatEntity
+}
+
+func (Chat) NewEntity() interface{} {
+	return new(ChatEntity)
+}
+
+func (tgChat *Chat) SetEntity(entity interface{}) {
+	if entity == nil {
+		tgChat.ChatEntity = nil
+	} else {
+		tgChat.ChatEntity = entity.(*ChatEntity)
+	}
+}
+
+type ChatEntity struct {
+	UserGroupID string `datastore:",index"` // Do index
+	TgChatEntityBase
+}
+
+func (entity *ChatEntity) Load(ps []datastore.Property) error {
+	return datastore.LoadStruct(entity, ps)
+}
+
+func (entity *ChatEntity) Save() (properties []datastore.Property, err error) {
+	if properties, err = datastore.SaveStruct(entity); err != nil {
+		return properties, err
+	}
+	if properties, err = entity.TgChatEntityBase.CleanProperties(properties); err != nil {
+		return
+	}
+	if properties, err = gaedb.CleanProperties(properties, map[string]gaedb.IsOkToRemove{
+		"TgChatInstanceID": gaedb.IsEmptyString,
+	}); err != nil {
+		return
+	}
+	return
+}
+
