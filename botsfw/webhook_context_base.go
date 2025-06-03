41c50bfe7f8c3245312d7554476939cbbcdadf68
Alexander Trakhimenok <alex@trakhimenok.com>
2025-06-03 21:17:57 +0100

    fix: moved into a dedicated file whContextDummy

--- botsfw/webhook_context_base.go
+++ botsfw/webhook_context_base.go
@@ -17,39 +17,6 @@ import (
 	"time"
 )
 
-var _ WebhookContext = (*whContextDummy)(nil)
-
-// whContextDummy is a dummy implementation of WebhookContext interface
-// It exists only to check what is NOT implemented by WebhookContextBase
-type whContextDummy struct {
-	*WebhookContextBase
-}
-
-func (w whContextDummy) TranslateWithMap(key string, args map[string]string) string {
-	panic(fmt.Sprintf("must be implemented in platform specific code: key=%s", key))
-}
-
-func (w whContextDummy) NewEditMessage(text string, format MessageFormat) (MessageFromBot, error) {
-	panic(fmt.Sprintf("must be implemented in platform specific code: text=%s, format=%v", text, format))
-}
-
-func (w whContextDummy) UpdateLastProcessed(chatEntity botsfwmodels.BotChatData) error {
-	panic(fmt.Sprintf("implement me in WebhookContextBase - UpdateLastProcessed(chatEntity=%v)", chatEntity))
-}
-
-func (w whContextDummy) AppUserData() (botsfwmodels.AppUserData, error) {
-	panic("implement me in WebhookContextBase") //TODO
-}
-
-func (w whContextDummy) IsNewerThen(chatEntity botsfwmodels.BotChatData) bool {
-	panic(fmt.Sprintf("implement me in WebhookContextBase - IsNewerThen(chatEntity=%v)", chatEntity))
-}
-
-func (w whContextDummy) Responder() WebhookResponder {
-	//TODO implement me
-	panic("implement me")
-}
-
 // WebhookContextBase provides base implementation of WebhookContext interface
 // TODO: Document purpose of a dedicated base struct (e.g. example of usage by developers)
 type WebhookContextBase struct {
